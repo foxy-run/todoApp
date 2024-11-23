df80d7d50b47f3b13e064b77a328599f908ae30b
Roxy <69159399+foxy-run@users.noreply.github.com>
2024-11-23 22:29:02 +0300

    add Edit button for tasks and SCSS, fix class naming and changing image after click on Done button

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 // Поиск элементов на странице
 const form = document.querySelector('#form');
 const input = document.querySelector('#add');
-const tasksList = document.querySelector('.tasks__list');
-// const emptyList = document.querySelector('#emptyList');
-//const editInput = document.querySelector('#task-input');
+const tasksList = document.querySelector('.tasks-list');
 
 // Массив для хранения в localStorage
 let tasks = [];
@@ -17,13 +15,13 @@ checkEmptyList();
 
 // Отрисовка задачи для каждого элемента, сохраненного в массиве
 tasks.forEach(task => {
-    const classCss = task.done ? 'tasks__title done' : 'tasks__title';
+    const classCss = task.done ? 'tasks-title done' : 'tasks-title';
     const taskHTML = `
-                <li class="tasks__item row my-1" id="${task.id}">
-                  <button type="button" class="tasks__done col-1 offset-1" data-action="done"><img src="assets/img/check.png" width="25px" height="25px" alt="Task done"></button>
-                  <input class="${classCss} col-8" disabled type="text" placeholder="${task.text}" id="task-input"/>
-                  <button type="button" class="tasks__edit col-1 " data-action="edit"><img src="assets/img/edit.png" width="25px" height="25px" alt="Task edit"></button>
-                  <button type="button" class="tasks__delete col-1" data-action="delete"><img src="assets/img/trash.png" width="25px" height="25px" alt="Task delete"></button>
+                <li class="tasks-item row my-1" id="${task.id}">
+                  <button type="button" class="tasks-done col-1 offset-1" data-action="done"><img src="assets/img/check.png" width="25px" height="25px" alt="Task done"></button>
+                  <input class="${classCss} col-8" disabled type="text" value="${task.text}" id="task-input"/>
+                  <button type="submit" class="tasks-edit col-1 " data-action="edit"><img src="assets/img/edit.png" width="25px" height="25px" alt="Task edit"></button>
+                  <button type="button" class="tasks-delete col-1" data-action="delete"><img src="assets/img/trash.png" width="25px" height="25px" alt="Task delete"></button>
                 </li>
     `;
     tasksList.insertAdjacentHTML('beforeend', taskHTML);
@@ -37,7 +35,7 @@ function saveToLocalStorage() {
 form.addEventListener('submit', addTask);
 tasksList.addEventListener('click', deleteTask);
 tasksList.addEventListener('click', doneTask);
-//tasksList.addEventListener('click', editTask);
+tasksList.addEventListener('click', editTask);
 
 function addTask(event) {
     // Отмена отправки формы
@@ -58,14 +56,14 @@ function addTask(event) {
     tasks.push(newTask);
 
     // Проверка статуса задачи
-    const classCss = 'tasks__title';
+    const classCss = 'tasks-title';
     // Отрисовка шаблона задачи
     const taskHTML = `
-                <li class="tasks__item row my-1" id="${newTask.id}">
-                  <button type="button" class="tasks__done col-1 offset-1" data-action="done"><img src="assets/img/check.png" width="25px" height="25px" alt="Task done"></button>
+                <li class="tasks-item row my-1" id="${newTask.id}">
+                  <button type="button" class="tasks-done col-1 offset-1" data-action="done"><img src="assets/img/check.png" width="25px" height="25px" alt="Task done"></button>
                   <input class="${classCss} col-8" disabled type="text" value="${newTask.text}" id="task-input"/>
-                  <button type="button" class="tasks__edit col-1 " data-action="edit"><img src="assets/img/edit.png" width="25px" height="25px" alt="Task edit"></button>
-                  <button type="button" class="tasks__delete col-1" data-action="delete"><img src="assets/img/trash.png" width="25px" height="25px" alt="Task delete"></button>
+                  <button type="button" class="tasks-edit col-1 " data-action="edit"><img src="assets/img/edit.png" width="25px" height="25px" alt="Task edit"></button>
+                  <button type="button" class="tasks-delete col-1" data-action="delete"><img src="assets/img/trash.png" width="25px" height="25px" alt="Task delete"></button>
                 </li>
     `;
 
@@ -85,7 +83,7 @@ function deleteTask(event) {
         return
     }
 
-    const parentNode = event.target.closest('.tasks__item'); // поиск ближайшего родителя
+    const parentNode = event.target.closest('.tasks-item'); // поиск ближайшего родителя
 
     // Нахождение id таски
     const id = parentNode.id;
@@ -100,17 +98,15 @@ function deleteTask(event) {
 
     // Вывод подтверждения об удалении
     const confirmDelete = confirm('Удалить задачу?')
-    if (confirmDelete ==false){
+    if (confirmDelete == false) {
         return;
-    } else{
+    } else {
         // Удаление задачи из массива
         tasks.splice(index, 1);
         parentNode.remove(); // удаление родителя
         checkEmptyList();
         saveToLocalStorage();
     }
-
-
 }
 
 function doneTask(event) {
@@ -119,7 +115,7 @@ function doneTask(event) {
         return
     }
 
-    const parentNode = event.target.closest('.tasks__item');    // поиск родительского тега
+    const parentNode = event.target.closest('.tasks-item');    // поиск родительского тега
 
     // поиск id таски
     const id = parentNode.id;
@@ -129,51 +125,64 @@ function doneTask(event) {
         }
     });
 
+    const taskTitle = parentNode.querySelector('.tasks-title'); // поиск текста задачи
+    const doneButton = parentNode.querySelector('.tasks-done img');
     task.done = !task.done;
-    const taskTitle = parentNode.querySelector('.tasks__title'); // поиск текста задачи
-    taskTitle.classList.toggle('done'); // добавление класса для визуализации выполнения задачи
+
+    if (task.done) {
+        taskTitle.classList.add('done'); // класс для выполненной таски
+        doneButton.src = 'assets/img/uncheck.png';
+        doneButton.alt = 'Uncheck task';
+    } else {
+        taskTitle.classList.remove('done');
+        doneButton.src = 'assets/img/check.png';
+        doneButton.alt = 'Task done';
+    }
+
     saveToLocalStorage();
 }
 
-// function editTask(event) {
-//     // Проверка, что был клик по кнопке done
-//     if (event.target.dataset.action !== 'edit') {
-//         return
-//     }
-//
-//     const parentNode = event.target.closest('.tasks__item');    // поиск родительского тега
-//
-//     // поиск id таски
-//     const id = parentNode.id;
-//     const task = tasks.find(function (task) {
-//         if (task.id == id) {
-//             return true
-//         }
-//     });
-//
-//     task.isEdit = !task.isEdit;
-//     const taskTitle = parentNode.querySelector('.tasks__title'); // поиск текста задачи
-//     taskTitle.classList.toggle('edit'); // добавление класса для визуализации выполнения задачи
-//
-//     if (task.isEdit == false) {
-//         return;
-//     }
-//
-//     // const editText = editInput.textContent;
-//
-//     let editRemove = document.getElementById('task-input');
-//     editRemove.removeAttribute('disabled');
-//     const valueFromInput = valueFromInput.value;
-//
-//     console.log(valueFromInput);
-//         //saveToLocalStorage();
-// }
+function editTask(event) {
+    // Проверка, что был клик по кнопке edit
+    if (event.target.dataset.action !== 'edit') {
+        return;
+    }
+
+    const parentNode = event.target.closest('.tasks-item'); // поиск родительского тега
+    const id = parentNode.id; // поиск id таски
+    const task = tasks.find(function (task) {
+        if (task.id == id) {
+            return true
+        }
+    })
+    const taskInput = parentNode.querySelector('#task-input'); // поиск текста задачи
+
+    if (!task.isEdit) {
+        // Включение режима редактирования
+        task.isEdit = true;
+        taskInput.removeAttribute('disabled');
+        taskInput.focus();
+        event.target.querySelector('img').src = 'assets/img/save.png';
+    } else {
+        const newTaskText = taskInput.value.trim(); // Получние нового текста
+        if (newTaskText !== '') {
+            task.text = newTaskText; // Обновление текста таски в массиве
+            taskInput.setAttribute('disabled', 'true');
+            task.isEdit = false;
+            event.target.querySelector('img').src = 'assets/img/edit.png';
+            saveToLocalStorage();
+        } else {
+            alert('Текст задачи не может быть пустым!');
+            taskInput.focus();
+        }
+    }
+}
 
 
 function checkEmptyList() {
     if (tasks.length === 0) {
-        const emptyListTasks = `<li class="tasks__item empty col-md-auto" id="emptyList">
-                  <div class="tasks__title_empty ">Список дел пока пуст...</div>
+        const emptyListTasks = `<li class="tasks-item empty col-md-auto" id="emptyList">
+                  <div class="tasks-title_empty ">Список дел пока пуст...</div>
                 </li>`
         tasksList.insertAdjacentHTML('afterbegin', emptyListTasks);
     }
